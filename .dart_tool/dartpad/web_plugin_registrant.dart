bcacb11ba90f21824f25f223659c7ca0a2cf7e58
Your Name <miftahfentawdev@gmail.com>
2025-12-06 09:35:07 +0300

    dart tools upgradded for with the packages

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -6,19 +6,19 @@
 // @dart = 2.13
 // ignore_for_file: type=lint
 
-import 'package:cloud_firestore_web/cloud_firestore_web.dart';
-import 'package:firebase_auth_web/firebase_auth_web.dart';
-import 'package:firebase_core_web/firebase_core_web.dart';
-import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:device_info_plus/src/device_info_plus_web.dart';
+import 'package:flutter_native_splash/flutter_native_splash_web.dart';
+import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:syncfusion_pdfviewer_web/pdfviewer_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
-  FirebaseFirestoreWeb.registerWith(registrar);
-  FirebaseAuthWeb.registerWith(registrar);
-  FirebaseCoreWeb.registerWith(registrar);
-  FirebaseStorageWeb.registerWith(registrar);
+  DeviceInfoPlusWebPlugin.registerWith(registrar);
+  FlutterNativeSplashWeb.registerWith(registrar);
+  FluttertoastWebPlugin.registerWith(registrar);
+  SyncfusionFlutterPdfViewerPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
